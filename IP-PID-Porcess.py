81cf64df147cf84d02b823c001c241551725eb6b
Younger <14861043+LiyangDotWebsite@users.noreply.github.com>
2021-01-17 16:30:20 +0800

    Add files via upload

---
+++ IP-PID-Porcess.py
@@ -0,0 +1,35 @@
+import psutil
+from time import sleep
+import logging
+
+logging.basicConfig(filename='example.log', encoding='utf-8', format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+def check_con(target_ip):
+	net_con = psutil.net_connections()
+	for i in net_con:
+		try:
+			ip = i[4][0]
+			stat = i[5]
+		except:
+			ip = ''
+			stat = ''
+		if ip == target_ip and stat=='ESTABLISHED':
+			pid = i[6]
+			logging.info("Found PID"+str(pid))
+			if psutil.pid_exists(pid):
+				for p in psutil.process_iter(['pid', 'name', 'username']):
+					if p.pid == pid:
+						logging.warning(p.info)
+			else:
+				print("Pid doesn't exist")
+			logging.info(str(i)+str(pid))
+
+if __name__=='__main__':
+	logging.info("Starting")
+	target_ip = '149.129.123.10'
+	logging.info("Target: " + target_ip)
+	print('Detecting the network connections to %s '% target_ip)
+	while True:
+		check_con(target_ip)
+		sleep(0.5)
+		logging.info('Not detected')
